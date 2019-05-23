7d1cb9e3347ddd2c1190acd9b05533c9ad6c9450
Lucas Carnevalli <verdi@hotmail.com.br>
2019-05-23 14:34:39 -0300

    init

---
+++ src/main/java/br/community/javaclean/JavacleanApplication.java
@@ -0,0 +1,13 @@
+package br.community.javaclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/community/javaclean/JavacleanApplicationTests.java
@@ -0,0 +1,16 @@
+package br.community.javaclean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavacleanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
