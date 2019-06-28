f75acbacec7d4303a53c58d741bbc3ae6519f67e
Lucas Carnevalli <verdi@hotmail.com.br>
2019-06-28 15:42:16 -0300

    security tests

---
+++ security-test/src/test/java/br/community/javaclean/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package br.community.javaclean;
+
+import static io.github.osvaldjr.EasyCucumberRunner.GLUE_EASY_CUCUMBER;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import io.github.osvaldjr.EasyCucumberRunner;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = {"pretty"},
+    features = {"src/test/resources/features"},
+    glue = {GLUE_EASY_CUCUMBER},
+    strict = true)
+public class RunCucumberTest extends EasyCucumberRunner {}
---
+++ security-test/src/test/resources/features/security.feature
@@ -0,0 +1,4 @@
+Feature: Security
+
+  Scenario: Validate security scan
+    Given import scan policy "javaclean.policy"
